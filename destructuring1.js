f9eba32a5d1fadc2a734b052a71d74ab5c014f21
MarcosSQ <marcosamap1978237@gmail.com>
2021-01-24 19:12:15 -0300

    Continuacao curso 24_01_2020

---
+++ destructuring1.js
@@ -0,0 +1,10 @@
+const pessoa = {
+    nome: 'Marcos Paiva',
+    idade: 42,
+    endereco: {
+        logradouro: 'Rua manoel Martins Sobrinho',
+        numero: 56
+    }
+}
+const {nome, idade ,endereco} = pessoa
+console.log(nome,endereco)
